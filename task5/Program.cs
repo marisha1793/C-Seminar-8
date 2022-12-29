fb8e849e6d9cd9c611bec0ee122f11447000448d
Mariya <marisha_1793@mail.ru>
2022-12-29 23:17:08 +0700

    task5

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07*/
+
+
+int[,] array = new int [4, 4];
+int [,] spiralArray = Create2DSpiralArray(array);
+Print2DArray(spiralArray);
+
+int[,] Create2DSpiralArray(int[,] array)
+{
+    int number = 1;
+    int i = 0;
+    int j = 0;
+    while (number <= array.GetLength(0) * array.GetLength(1))
+    {
+        array[i, j] = number;
+        number++;
+        if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+        {
+            j++;
+        }
+        else if (i < j && i + j >= array.GetLength(0) - 1)
+        {
+            i++;
+        }
+        else if (i >= j && i + j > array.GetLength(1) - 1)
+        {
+            j--;
+        }
+        else
+        {
+            i--;
+        }
+    }
+
+    return array;
+}
+
+void Print2DArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($"0{array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
